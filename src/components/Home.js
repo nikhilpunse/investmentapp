7d7dab7537342dcbfaf62a109b360ab1a8bace82
Nikhil Punse <nikhil.punse5@gmail.com>
2023-02-28 14:03:07 +0530

    corousel updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -99,16 +99,19 @@ We have built the largest global community of professional FinTech investors all
             <p className='mb-16'>We are the industry leader in establishing an innovation-friendly organization, developing new <br/> business models and new products.The company is on the cutting edge of new technologies.
             </p>
               {/* Carosoul1 */}
-        <div></div>
-        <div className='w-full'>
-          <div
-            id="carouselExampleControls"
-            className="carousel slide relative"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-inner relative w-full overflow-hidden">
-              <div className="carousel-item active relative float-left w-full">
-                {/* Custom code */}
+              
+              <div
+  id="carouselExampleControls"
+  className="relative w-full"
+  data-te-carousel-init
+  data-te-carousel-slide>
+  <div
+    className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+    <div
+      className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      data-te-carousel-item
+      data-te-carousel-active>
+       {/* Custom code */}
                  {/* corousel content */}
                  <div className='flex flex-col items-center'>
             <div className='w-full flex justify-evenly'>
@@ -117,62 +120,88 @@ We have built the largest global community of professional FinTech investors all
                     <HomeCard/>
             </div>   
 
-            <button className='font-semibold px-8 py-2 my-12 bg-gradient-to-r from-green-300 to-blue-600 rounded-xl border'>See all Business</button>     
-             </div> 
-              </div>
-              <div className="carousel-item relative float-left w-full">
-                {/* Custom code */}
+            <button className='font-semibold px-8 py-2 my-12 bg-gradient-to-r from-green-300 to-blue-600 rounded-xl border'>See all Business</button>
+	</div> 
+    </div>
+    <div
+      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      data-te-carousel-item>
+         {/* Custom code */}
                  {/* corousel content */}
                  <div className='flex flex-col items-center'>
-                  <div className='w-full flex justify-evenly'>
-                          <HomeCard/>
-                          <HomeCard/>
-                          <HomeCard/>
-                  </div>   
-
-                  <button className='font-semibold px-8 py-1 my-12 bg-gradient-to-r from-green-300 to-blue-600 rounded-xl border'>See all Business</button>     
-                </div>  
-              </div>
-              <div className="carousel-item relative float-left w-full">
-              {/* Custom code */}
-               {/* corousel content */}
-               <div className='flex flex-col items-center'>
             <div className='w-full flex justify-evenly'>
                     <HomeCard/>
                     <HomeCard/>
                     <HomeCard/>
             </div>   
 
-            <button className='font-semibold px-8 py-1 my-12 bg-gradient-to-r from-green-300 to-blue-600 rounded-xl border'>See all Business</button>     
-            </div>
-              </div>
-            </div>
-            <button
-              className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon inline-block bg-no-repeat"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon inline-block bg-no-repeat"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-        </div>
+            <button className='font-semibold px-8 py-2 my-12 bg-gradient-to-r from-green-300 to-blue-600 rounded-xl border'>See all Business</button>
+	</div> 
+
+    </div>
+    <div
+      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      data-te-carousel-item>
+       {/* Custom code */}
+                 {/* corousel content */}
+                 <div className='flex flex-col items-center'>
+            <div className='w-full flex justify-evenly'>
+                    <HomeCard/>
+                    <HomeCard/>
+                    <HomeCard/>
+            </div>   
+
+            <button className='font-semibold px-8 py-2 my-12 bg-gradient-to-r from-green-300 to-blue-600 rounded-xl border'>See all Business</button>
+	</div> 
+    </div>
+  </div>
+  <button
+    className="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+    type="button"
+    data-te-target="#carouselExampleControls"
+    data-te-slide="prev">
+    <span className="inline-block h-8 w-8">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="h-6 w-6">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 19.5L8.25 12l7.5-7.5" />
+      </svg>
+    </span>
+    <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      >Previous</span>
+  </button>
+  <button
+    className="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+    type="button"
+    data-te-target="#carouselExampleControls"
+    data-te-slide="next">
+    <span className="inline-block h-8 w-8">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="h-6 w-6">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+      </svg>
+    </span>
+    <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      >Next</span>
+  </button>
+</div>
         </section>
 
 
@@ -211,34 +240,41 @@ We have built the largest global community of professional FinTech investors all
             </p>
 
             {/* Carosoul2 */}
-            <div className='w-full'>
-      <div id="carouselExampleIndicators" className="carousel slide relative" data-bs-ride="carousel">
-  <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+            <div
+  id="carouselExampleIndicators"
+  className="relative w-full"
+  data-te-carousel-init
+  data-te-carousel-slide>
+  <div
+    className="invisible absolute right-0 bottom-0 left-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+    data-te-carousel-indicators>
     <button
       type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="0"
-      className="active invisible"
+      data-te-target="#carouselExampleIndicators"
+      data-te-slide-to="0"
+      data-te-carousel-active
+      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
       aria-current="true"
-      aria-label="Slide 1"
-    ></button>
+      aria-label="Slide 1"></button>
     <button
       type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="1"
-      className='invisible'
-      aria-label="Slide 2"
-    ></button>
+      data-te-target="#carouselExampleIndicators"
+      data-te-slide-to="1"
+      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+      aria-label="Slide 2"></button>
     <button
       type="button"
-      data-bs-target="#carouselExampleIndicators"
-      data-bs-slide-to="2"
-      className='invisible'
-      aria-label="Slide 3"
-    ></button>
+      data-te-target="#carouselExampleIndicators"
+      data-te-slide-to="2"
+      className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+      aria-label="Slide 3"></button>
   </div>
-  <div className="carousel-inner relative w-full overflow-hidden">
-    <div className="carousel-item active float-left w-full">
+  <div
+    className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+    <div
+      className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      data-te-carousel-item
+      data-te-carousel-active>
       {/* custome code */}
        {/* Carosoul2 element */}
        <div className='flex justify-evenly w-full'>
@@ -247,38 +283,76 @@ We have built the largest global community of professional FinTech investors all
                 <HomeCard2/>
             </div>
     </div>
-    <div className="carousel-item float-left w-full">
-      
-     {/* custome code */}
-      {/* Carosoul2 element */}
-      <div className='flex justify-evenly w-full'>
+    <div
+      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      data-te-carousel-item>
+      {/* custome code */}
+       {/* Carosoul2 element */}
+       <div className='flex justify-evenly w-full'>
                 <HomeCard2/>
                 <HomeCard2/>
                 <HomeCard2/>
             </div>
     </div>
-    
+    <div
+      className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+      data-te-carousel-item>
+      {/* custome code */}
+       {/* Carosoul2 element */}
+       <div className='flex justify-evenly w-full'>
+                <HomeCard2/>
+                <HomeCard2/>
+                <HomeCard2/>
+            </div>
+    </div>
   </div>
   <button
-    className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+    className="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
     type="button"
-    data-bs-target="#carouselExampleIndicators"
-    data-bs-slide="prev"
-  >
-    <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
+    data-te-target="#carouselExampleIndicators"
+    data-te-slide="prev">
+    <span className="inline-block h-8 w-8">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="h-6 w-6">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 19.5L8.25 12l7.5-7.5" />
+      </svg>
+    </span>
+    <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      >Previous</span>
   </button>
   <button
-    className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+    className="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
     type="button"
-    data-bs-target="#carouselExampleIndicators"
-    data-bs-slide="next"
-  >
-    <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
+    data-te-target="#carouselExampleIndicators"
+    data-te-slide="next">
+    <span className="inline-block h-8 w-8">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="h-6 w-6">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+      </svg>
+    </span>
+    <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+      >Next</span>
   </button>
 </div>
-    </div>
            
         </section>
 
@@ -392,7 +466,6 @@ We have built the largest global community of professional FinTech investors all
           <img className='absolute w-[680px] h-[680px] -top-52 -left-56 ' src={img5}/>
         </section>
 
-        
     </div>
   )
 }
